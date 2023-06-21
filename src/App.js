cb6371778e9e8634ad6248645f1b1ac020eb823d
Vamshi Reddy <vamshiproject02@gmail.com>
2023-06-21 22:20:54 +0530

    setting up react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import axios from 'axios';
+import './App.css'
+
+class App extends React.Component{
+  state ={ advice: ''};
+
+  componentDidMount(){
+    console.log('component did mount')
+  }
+
+  render() {
+    return(
+      <h1>APP</h1>
+    );
+  }
+}
+export default App;
